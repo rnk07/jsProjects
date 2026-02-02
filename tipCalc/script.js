912361ec8f1dc6df3323bb853a3f597f6cc70280
rnk07 <patelrnk07@gmail.com>
2026-02-01 21:51:12 -0500

    added html,js,css code

---
+++ tipCalc/script.js
@@ -0,0 +1,29 @@
+const billAmt = document.querySelector(".bill-field")
+const tipPercent = document.querySelector(".tip-field")
+const calculateBtn = document.querySelector(".cal-btn")
+
+const totalPlaceholder = document.querySelector(".total-value")
+
+calculateBtn.addEventListener("click", calculateTip);
+
+
+
+function calculateTip() {
+
+    const bill = Number(billAmt.value);
+    const tip = Number(tipPercent.value);
+
+    if (!bill || !tip) {
+        console.log("Please enter bill and tip");
+         totalPlaceholder.textContent = 0;
+    }
+
+    const finalTipAmt = (bill * tip) / 100;
+    const total = bill + finalTipAmt;
+
+    console.log("Tip:", finalTipAmt);
+    console.log("Total:", total);
+
+    totalPlaceholder.textContent = total.toFixed(2)
+
+}
